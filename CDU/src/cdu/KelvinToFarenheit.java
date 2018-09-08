6491987f6281683dc0c661ed3bf9151435221859
Angelarmandol <Angelarmandol@hotmail.com>
2018-09-08 15:23:31 -0700

    add Kelvin a Farenheit

---
+++ CDU/src/cdu/KelvinToFarenheit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdu;
+
+/**
+ *
+ * @author ANGEL
+ */
+public class KelvinToFarenheit {
+    
+    
+    
+    public double KelvinToFarenheit (double value){
+       double farenheit =((value-273.15)*1.8)+32;
+       return farenheit;
+    }
+    
+}
