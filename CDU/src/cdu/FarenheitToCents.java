d1d20ac0df2dc3b4f41a4bc17c98087e373b2470
Angelarmandol <Angelarmandol@hotmail.com>
2018-09-08 15:04:25 -0700

    add centToFar class

---
+++ CDU/src/cdu/FarenheitToCents.java
@@ -0,0 +1,18 @@
+package cdu;
+
+/**
+ *
+ * @author ANGEL
+ */
+public class FarenheitToCents {
+    
+    
+    public float FarenheitToCent(float value){
+        float farenheit=value*2-value/5;
+        farenheit=farenheit+32;
+        return farenheit;
+        //
+    }
+    
+
+}
