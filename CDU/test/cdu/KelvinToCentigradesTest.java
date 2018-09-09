dc650ba0c05f15613fd38ebfed6dfca5450ecf9d
Angelarmandol <Angelarmandol@hotmail.com>
2018-09-08 21:32:43 -0700

    add KelvinToCentigradesTest  test class

---
+++ CDU/test/cdu/KelvinToCentigradesTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdu;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ANGEL
+ */
+public class KelvinToCentigradesTest {
+    
+    @Test
+    public void testMethod() {
+        System.out.println("method");
+        double value = 10;
+        KelvinToCentigrades instance = new KelvinToCentigrades();
+        double expResult = -263.15;
+        double result = instance.method(value);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of menu method, of class KelvinToCentigrades.
+     */
+ 
+    
+}
